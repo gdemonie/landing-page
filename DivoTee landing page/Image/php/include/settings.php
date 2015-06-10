b321548f73c16180f41e0620439e48d3d1714081
Guillaume de Monie <guillaumedemonie@gmail.com>
2015-06-10 10:05:18 +0200

    rails new

---
+++ DivoTee landing page/Image/php/include/settings.php
@@ -0,0 +1,20 @@
+<?php
+
+	// MailChimp
+	$listID = "LIST-ID";
+	$APIKey = "API-KEY";
+
+	// Database
+	$host = 'YOUR-HOST';
+	$username = 'YOUR-USER';
+	$password = 'YOUR-PASSWORD';
+	$database = 'TEMPLATE-DATABASE';
+
+	// Contact
+	$to = 'contact@golfdivotee.com';
+    $subject = 'Subject here...';
+
+	// Variant
+	$useMailChimp = "YES";
+
+?>
